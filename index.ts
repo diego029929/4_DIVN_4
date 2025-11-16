881151b50b04a0410f52d21bf6a000de28353ec7
Official <diegoferraro@free.fr>
2025-11-16 22:50:56 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,27 @@
+// app/components/index.ts
+
+export { default as Header } from "./header";
+// ou si votre fichier est Header.tsx / Header/index.tsx
+// export { default as Header } from "./Header";
+
+export { default as Footer } from "./footer";
+// export { default as Footer } from "./Footer";
+
+export { default as AddToCartForm } from "./add-to-cart-form";
+// export { default as AddToCartForm } from "./AddToCartForm";
+
+export { default as CartContent } from "./cart-content";
+// export { default as CartContent } from "./CartContent";
+
+export { default as CheckoutForm } from "./checkout-form";
+// export { default as CheckoutForm } from "./CheckoutForm";
+
+export { default as ProductCard } from "./product-card";
+// export { default as ProductCard } from "./ProductCard";
+
+export { default as Card } from "./ui/card";
+// export { default as Card } from "./UI/Card";
+
+export { default as Button } from "./ui/button";
+// export { default as Button } from "./UI/Button";
+
