56ec68cf50a605b1161cb3b0f83d25abd0be60ad
Official <diegoferraro@free.fr>
2025-12-02 23:26:27 +0100

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,10 +2,6 @@
 
 export const dynamic = "force-dynamic"; // Empêche tout prerender
 
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
-import { CartContent } from "@/components/cart-content";
-
 export default function CartPage() {
   return (
     <div className="min-h-screen flex flex-col">
