c0b5dab0f886330b3969907fc76b6c77aa27f6bf
Official <diegoferraro@free.fr>
2025-12-02 23:47:51 +0100

    Update layout.tsx

--- app/cgv/layout.tsx
+++ app/cgv/layout.tsx
@@ -1,7 +1,11 @@
+import CartProvider from "@/app/components/cart-provider";
+
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {children}
-    </div>
+    <CartProvider>
+      <div>
+        {children}
+      </div>
+    </CartProvider>
   );
 }
