b30eb729a9f6deb940284e4dfd99609f602865a7
Official <diegoferraro@free.fr>
2025-12-04 00:05:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
+"use client";
 
-
+import "./globals.css";
 import { CartProvider } from "@/context/cart-context";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
         <CartProvider>
-          <Header />
-          {children}
-          <Footer />
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </CartProvider>
       </body>
     </html>
