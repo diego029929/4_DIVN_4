f10caac164dc100118eb66aba3687ee9fbc40a38
Official <diegoferraro@free.fr>
2025-12-03 23:55:14 +0100

    Update layout.tsx

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,11 +1,13 @@
 import { CartProvider } from "@/context/cart-context";
 
-export default function AboutLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
-    <CartProvider>
-      <div>
-        {children}
-      </div>
-    </CartProvider>
+    <html lang="en">
+      <body>
+        <CartProvider>
+          {children}
+        </CartProvider>
+      </body>
+    </html>
   );
 }
