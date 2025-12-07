a43d7643d6b101b0966d79506e26a49935c53342
Official <diegoferraro@free.fr>
2025-12-07 23:20:44 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "app/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
