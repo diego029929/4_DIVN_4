d89dc43f54fa8a9b66c8e63b1d50e8d345cc5f91
Official <diegoferraro@free.fr>
2025-12-08 22:58:57 +0100

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -64,7 +64,7 @@ export function Header() {
 
       {/* IMAGE PRINCIPALE */}
       <div className="main-image">
-        <img src="/luxury-fashion-dark-minimalist.jpg" alt="Image principale" />
+        <img src="image1jpg"pg" alt="Image principale" />
       </div>
     </>
   );
