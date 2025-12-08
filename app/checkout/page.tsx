e25636ffb7d49d2a139a531ec036c3bef13b31b8
Official <diegoferraro@free.fr>
2025-12-08 23:21:31 +0100

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Header } from "@/components/header";
+import  Header  from "@/components/header";
 import { Footer } from "@/components/footer";
 import { CartProvider } from "@/context/cart-context";
 import { CheckoutForm } from "@/components/checkout-form";
