b1ea11ebceacc2f5cee0497e9de51273f5b8b2c7
Official <diegoferraro@free.fr>
2025-12-09 22:42:30 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ProductCard } from "@/components/product-card";
 
 export default function ShopPage() {
   return (
-    <main className="pt-20 bg-white min-h-screen">
+    <main className="pt-20 bg-neutral-100 min-h-screen">
 
       {/* MAIN IMAGE */}
       <div className="w-full">
