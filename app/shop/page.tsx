f12d3a48a0f478e23c24751b02a20ef93eee4fa3
Official <diegoferraro@free.fr>
2025-12-10 11:54:27 +0100

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,7 +2,7 @@
 
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { CartProvider } from "@/context/cart-context";
+import { CartProvider } from "@/components/cart-provider";
 import ProductsGrid from "@/components/products-grid"; // exemple
 
 export const dynamic = "force-dynamic";
