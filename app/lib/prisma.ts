a28c0c7f83dc585c8350c4f5b13de61965faf8ad
Official <diegoferraro@free.fr>
2025-12-14 13:13:52 +0100

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,13 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 // Gestion globale pour éviter plusieurs instances en dev
 declare global {
-  var prisma: PrismaClient | undefined;
+  // Utilise InstanceType pour typer correctement
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
 }
 
+// Crée l’instance si elle n’existe pas
 export const prisma =
   global.prisma ??
   new PrismaClient({
     log: ["error"],
   });
 
+// En dev, stocke l’instance globale pour HMR
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
