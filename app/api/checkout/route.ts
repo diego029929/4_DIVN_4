de6f9236b57df3560bef488f6312d02c692d750c
Official <diegoferraro@free.fr>
2025-12-14 22:35:04 +0100

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { cookies } from "next/headers";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-14",
+  apiVersion: "2023-10-16",
 });
 
 export async function POST(req: Request) {
