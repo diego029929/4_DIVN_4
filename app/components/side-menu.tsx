78062fd5448313399b327f5dca64f83ace2238c0
Official <diegoferraro@free.fr>
2025-12-16 23:43:13 +0100

    Update side-menu.tsx

--- app/components/side-menu.tsx
+++ app/components/side-menu.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { X } from "lucide-react";
-import { FaTiktok, FaInstagram, FaXTwitter } from "react-icons/fa6";
 
 type SideMenuProps = {
   open: boolean;
@@ -54,17 +53,19 @@ export default function SideMenu({ open, onClose }: SideMenuProps) {
         </div>
 
         {/* Social icons (EXACT) */}
-        <div className="absolute bottom-10 left-6 flex gap-6 text-2xl">
-          <a href="https://www.tiktok.com" target="_blank">
-            <FaTiktok />
-          </a>
-          <a href="https://www.instagram.com" target="_blank">
-            <FaInstagram />
-          </a>
-          <a href="https://twitter.com" target="_blank">
-            <FaXTwitter />
-          </a>
-        </div>
+<div className="absolute bottom-10 left-6 flex gap-6 text-white">
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M12.34 2c1.2 0 2.38.35 3.37 1.01l.36.24-2.36 2.36a3.96 3.96 0 00-1.37-.25A4 4 0 008 9.36v5.28a4 4 0 104.34-3.98v-2.8l3.14-3.14v6.07A7 7 0 1112.34 2z" />
+  </svg>
+
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm5 5a5 5 0 100 10 5 5 0 000-10zm6.5-.9a1.1 1.1 0 11-2.2 0 1.1 1.1 0 012.2 0z" />
+  </svg>
+
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M18.9 2L13.2 8.6 19.8 22h-3.9l-4.5-9.2L5.8 22H2l7.1-8.3L2.6 2h4l4.1 8.4L16 2h2.9z" />
+  </svg>
+</div>
       </aside>
     </>
   );
