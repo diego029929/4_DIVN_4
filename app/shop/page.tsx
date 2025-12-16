19922658d0391766001a2abbc768f54be09532a0
Official <diegoferraro@free.fr>
2025-12-16 16:10:07 +0100

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Header } from "@/components/header";
+import  Header  from "@/components/header";
 import { Footer } from "@/components/footer";
 import { CartProvider } from "@/components/cart-provider";
 import ProductsGrid from "@/components/products-grid"; // exemple
