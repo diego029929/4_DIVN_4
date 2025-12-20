5f436dd91b17d4b9e577e9951e1e755f2b452ead
Official <diegoferraro@free.fr>
2025-12-20 20:11:54 +0100

    Update init-db.ts

--- prisma/init-db.ts
+++ prisma/init-db.ts
@@ -1,14 +1,15 @@
-import { prisma } from "../app/lib/prisma"; // ajuste selon ton projet
+import { prisma, sql } from "../app/lib/prisma"; // ajuste selon ton projet
 
 async function main() {
-  await prisma.$executeRaw(`
-    CREATE TABLE IF NOT EXISTS "User" (
+  await prisma.$executeRaw(
+    sql`CREATE TABLE IF NOT EXISTS "User" (
       id SERIAL PRIMARY KEY,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL,
       "createdAt" TIMESTAMP DEFAULT NOW()
-    );
-  `);
+    );`
+  );
+
   console.log("✅ Table User créée ou existait déjà");
 }
 
