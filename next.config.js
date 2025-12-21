28b7ac91429185413a6a84b121b2de708e279fcd
Official <diegoferraro@free.fr>
2025-12-21 01:46:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-// next.config.js
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
-  // 🔴 IMPORTANT POUR RENDER
   eslint: {
     ignoreDuringBuilds: true
   },
@@ -15,7 +13,6 @@ const nextConfig = {
   },
 
   webpack: (config) => {
-    // Alias @ vers la racine du projet
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   }
