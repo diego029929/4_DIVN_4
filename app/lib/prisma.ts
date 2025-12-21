7574f569c7ee99a5d6be3c367998b3b5e8bfa167
Official <diegoferraro@free.fr>
2025-12-21 02:17:05 +0100

    Update and rename prisma.js to prisma.ts

---
+++ app/lib/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // Pour éviter plusieurs instances de Prisma en dev (Hot Reload)
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
