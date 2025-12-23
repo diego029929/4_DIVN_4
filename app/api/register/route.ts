6a1320d5f3e5f5f7a09d6c53d2cfe3c17a2f08de
Official <diegoferraro@free.fr>
2025-12-23 19:13:37 +0100

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/Prisma";
 import bcrypt from "bcryptjs";
 import { randomUUID } from "crypto";
 import { sendEmail } from "@/lib/email";
