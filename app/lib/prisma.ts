f8d5eee471ad4088d7c6686c9310c39eb3ddf0f3
Official <diegoferraro@free.fr>
2025-12-23 19:30:06 +0100

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,20 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // Pour éviter plusieurs instances de Prisma en dev (Hot Reload)
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
-export { prisma };
