e56b05aa3e82cd8410784ed21b3cd8cd551e3b86
Official <diegoferraro@free.fr>
2025-12-27 01:47:09 +0100

    Update route.ts

--- app/api/test-email/route.ts
+++ app/api/test-email/route.ts
@@ -2,32 +2,14 @@ import nodemailer from "nodemailer";
 
 export const runtime = "nodejs";
 
+import { sendEmail } from "@/lib/email";
+
 export async function GET() {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_SMTP_USER!,
-        pass: process.env.BREVO_SMTP_PASS!,
-      },
-      connectionTimeout: 10_000,
-    });
+  await sendEmail(
+    "TON_EMAIL@gmail.com",
+    "TEST BREVO API ✅",
+    "Si tu lis ça, C’EST FINI."
+  );
 
-    await transporter.sendMail({
-      from: "no-reply@brevo.com",
-      to: "wist.infodev@gmail.com",
-      subject: "TEST BREVO RENDER",
-      text: "Si tu reçois ça, BREVO + RENDER OK",
-    });
-
-    return new Response("EMAIL ENVOYÉ ✅");
-  } catch (err: any) {
-    console.error("SMTP_ERROR:", err);
-    return new Response(
-      "ERREUR SMTP ❌ : " + err.message,
-      { status: 500 }
-    );
-  }
+  return new Response("OK");
 }
