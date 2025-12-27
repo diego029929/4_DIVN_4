7a646a5a6392e046c025204aee778b6b9dc90f2e
Official <diegoferraro@free.fr>
2025-12-27 01:46:04 +0100

    Update email.ts

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -1,32 +1,35 @@
-// /lib/email.ts
-import nodemailer from "nodemailer";
-
 export async function sendEmail(
   to: string,
   subject: string,
   text: string
 ) {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_SMTP_USER,
-        pass: process.env.BREVO_SMTP_PASS,
+    const res = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "api-key": process.env.BREVO_API_KEY!,
       },
+      body: JSON.stringify({
+        sender: {
+          name: "DIVN",
+          email: "no-reply@brevo.com"
+        },
+        to: [{ email: to }],
+        subject,
+        textContent: text,
+      }),
     });
 
-    await transporter.sendMail({
-      from: `"DIVN" <no-reply@brevo.com>`, // ✅ SMTP partagé
-      to,
-      subject,
-      text,
-    });
+    if (!res.ok) {
+      const err = await res.text();
+      throw new Error(err);
+    }
 
     console.log("EMAIL ENVOYÉ ✅");
   } catch (err) {
     console.error("EMAIL_ERROR:", err);
   }
-}
-
+  }
+        
