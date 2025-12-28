33f99112cb15563a044994df6f80e24edfcc0d14
Official <diegoferraro@free.fr>
2025-12-28 18:06:24 +0100

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/api/auth/[...nextauth]/route"
 import { stripe } from "@/lib/stripe"
 
 export const runtime = "nodejs"
