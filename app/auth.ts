9cc90e5b8b5b2590a9943429c18f5996e082d3ef
Official <diegoferraro@free.fr>
2025-12-28 18:22:33 +0100

    Create auth.ts

---
+++ app/auth.ts
@@ -0,0 +1,38 @@
+// auth.ts
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { prisma } from "@/lib/prisma"
+import bcrypt from "bcryptjs"
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (!user) return null
+
+        const valid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        )
+
+        if (!valid) return null
+
+        return {
+          id: user.id,
+          email: user.email,
+        }
+      },
+    }),
+  ],
+})
