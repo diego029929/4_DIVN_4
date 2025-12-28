2ae296c4c450348640125dfaf4cea528be6ec27f
Official <diegoferraro@free.fr>
2025-12-28 02:19:25 +0100

    Update route.ts

--- app/api/check-session/route.ts
+++ app/api/check-session/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { stripe } from "@/lib/stripe";
 import { getServerSession } from "next-auth/next";
-import { handler as authHandler } from "./api/auth/[...nextauth]"; // ton NextAuth existant
+import { handler as authHandler } from "app/api/auth/[...nextauth]"; // ton NextAuth existant
 
 export const runtime = "nodejs";
 
