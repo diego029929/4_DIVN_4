0f28a356c79ad8bc1859b9074942a87fc2bc02e2
Official <diegoferraro@free.fr>
2025-12-31 01:06:51 +0100

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { randomUUID } from "crypto";
-import { renderVerifyEmail } from "@/lib/email-renderer.ts";
+import { renderVerifyEmail } from "@/lib/email-renderer.tsx";
 
 export async function POST(req: Request) {
   try {
