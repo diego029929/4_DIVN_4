3e3b2f19103f721530c7625738b7b28d6b2e1470
Official <diegoferraro@free.fr>
2025-12-31 01:53:56 +0100

    Update email-renderer.tsx

--- app/lib/email-renderer.tsx
+++ app/lib/email-renderer.tsx
@@ -1,14 +1,14 @@
 // lib/email-renderer.tsx
-import "server-only";
 import { render } from "@react-email/render";
-
 import VerifyAccountEmail from "@/emails/verify-account";
 import ResetPasswordEmail from "@/emails/reset-password";
 
+// Génère le HTML de l'email de vérification
 export function renderVerifyEmail(username: string, verifyUrl: string) {
   return render(<VerifyAccountEmail username={username} verifyUrl={verifyUrl} />);
 }
 
+// Génère le HTML de l'email de réinitialisation
 export function renderResetPasswordEmail(resetUrl: string) {
   return render(<ResetPasswordEmail resetUrl={resetUrl} />);
 }
