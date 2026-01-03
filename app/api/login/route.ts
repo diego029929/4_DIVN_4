f9c09003a11773b01c01018cee0213d8a1ee2e96
Official <diegoferraro@free.fr>
2026-01-03 17:17:08 +0100

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -60,7 +60,5 @@ export async function POST(req: Request) {
       { status: 500 }
     );
   }
-}
-
                                                                      }
                                
