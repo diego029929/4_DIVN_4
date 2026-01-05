f462ec2614e521a51ca42246a7b893d961c51e1c
Official <diegoferraro@free.fr>
2026-01-05 23:09:45 +0100

    Update route.ts

--- app/api/check-session/route.ts
+++ app/api/check-session/route.ts
@@ -1,15 +1,16 @@
-import { auh } from "@/auth"
-import { authOptions } from "@/auth"; // ⚠️ adapte le chemin si besoin
+import { NextResponse } from "next/server"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/auth" // adapte le chemin si besoin
 
 export async function GET() {
   try {
-    const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions)
 
     if (!session || !session.user) {
       return NextResponse.json(
         { user: null },
         { status: 401 }
-      );
+      )
     }
 
     return NextResponse.json({
@@ -17,12 +18,12 @@ export async function GET() {
         id: session.user.id,
         email: session.user.email,
       },
-    });
+    })
   } catch (err) {
-    console.error("CHECK_SESSION_ERROR", err);
+    console.error("CHECK_SESSION_ERROR", err)
     return NextResponse.json(
       { user: null },
       { status: 500 }
-    );
+    )
   }
-}
+  }
