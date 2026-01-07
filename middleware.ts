88f39370fe3c92ab16149feb8c748c8cba1becda
Official <diegoferraro@free.fr>
2026-01-07 23:32:17 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,15 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized: ({ token, req }) => {
-      // Zone admin
-      if (req.nextUrl.pathname.startsWith("/admin")) {
-        return token?.id !== undefined;
+      // 🔓 autorise admin/orders sans check
+      if (req.nextUrl.pathname === "/admin/orders") {
+        return true;
       }
+
+      if (req.nextUrl.pathname.startsWith("/admin")) {
+        return !!token;
+      }
+
       return true;
     },
   },
