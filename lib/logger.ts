1d74b88a05f9e7ea7b673d57e6fcaf72e19839ef
Official <diegoferraro@free.fr>
2026-01-09 14:58:37 +0100

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,8 +1,18 @@
 import pino from "pino";
+import send from "pino-http-send";
 
-export const logger = pino({
-  level: process.env.NODE_ENV === "production" ? "info" : "debug",
-  base: {
-    env: process.env.NODE_ENV,
+const betterStackUrl = process.env.BETTER_STACK_URL!; // ton endpoint HTTPS
+
+export const logger = pino(
+  {
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
   },
-});
+  send({
+    url: betterStackUrl,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.BETTER_STACK_TOKEN}`, // si Better Stack demande un token
+    },
+  })
+);
