bb482fe4deb9b4d2e90e714ba14d57646a17118c
Official <diegoferraro@free.fr>
2026-01-10 23:55:33 +0100

    Update route.ts

--- app/api/sentry-test/route.ts
+++ app/api/sentry-test/route.ts
@@ -1,3 +1,7 @@
 export async function GET() {
+  if (process.env.NODE_ENV === 'production') {
+    return new Response('Sentry test disabled in prod', { status: 200 });
+  }
+
   throw new Error('Test Sentry Backend');
 }
