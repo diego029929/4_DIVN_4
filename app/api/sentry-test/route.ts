88ca0f6cbded4c89a6724186965742a27201ae54
Official <diegoferraro@free.fr>
2026-01-14 22:59:48 +0100

    Delete app/api/sentry-test directory

--- app/api/sentry-test/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/sentry-test/route.ts
-import * as Sentry from "@sentry/nextjs";
-
-export const runtime = "nodejs";
-
-export async function GET() {
-  Sentry.captureMessage("🔥 SENTRY BACKEND OK 🔥");
-  return new Response("ok");
-}
