5e93b40a37a0a24b99f7e086adb2cee35d564827
Official <diegoferraro@free.fr>
2026-01-14 08:04:50 +0100

    Update instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,8 +1,16 @@
 import * as Sentry from "@sentry/nextjs";
 
 export function register() {
+  console.log("🔥 SENTRY INSTRUMENTATION REGISTER");
+
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+    // En prod mets 0.1, pour debug 1.0
     tracesSampleRate: 1.0,
+
+    environment: process.env.NODE_ENV,
+
+    debug: true, // ⚠️ TEMPORAIRE : logs dans Render
   });
 }
