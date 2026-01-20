7fcfb43b7a7ac3928ee816ff18f9fbb69ec00ade
Official <diegoferraro@free.fr>
2026-01-20 08:01:35 +0100

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,8 @@
 import { Logtail } from "@logtail/node";
 
 export const logtail = new Logtail(
-  process.env.BETTERSTACK_SOURCE_TOKEN!
+  process.env.BETTERSTACK_SOURCE_TOKEN!,
+  {
+    endpoint: "https://s1681247.eu-nbg-2.betterstackdata.com", // ✅ OBLIGATOIRE
+  }
 );
